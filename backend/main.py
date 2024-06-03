b4aae05371127c6004c0bca21c95fc828b766625
TJ-Liebsch <tjliebsch@outlook.com>
2024-06-03 06:15:23 -0500

    Finished project

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel, HttpUrl
 from fastapi.middleware.cors import CORSMiddleware
+import toml
 
 from services.genai import (
     YoutubeProcessor,
@@ -22,62 +23,31 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
 @app.post("/analyze_video")
 def analyze_video(request: VideoAnalysisRequest):
-    # Doing the analysis
-    processor = YoutubeProcessor()
-    result = processor.retrieve_youtube_documents(str(request.youtube_link), verbose=True)
+    with open("secrets.toml", "r") as s:
+        secrets = toml.load(s)
+
+    project = secrets["google_cloud"]["project_id"]
 
     genai_processor = GeminiProcessor(
         model_name = "gemini-pro",
-        project = "ai-dev-cqc-q1-2024"
+        project = project
     )
 
-    summary = genai_processor.generate_document_summary(result, verbose=True)
+    # Doing the analysis
+    processor = YoutubeProcessor(genai_processor=genai_processor)
+    result = processor.retrieve_youtube_documents(str(request.youtube_link), verbose=True)
 
+
+    # summary = genai_processor.generate_document_summary(result, verbose=True)
+    
+    # It did not recognize group_size as a variable when it was a parameter
+    group_size = 6
+
+    # Find key concepts
+    key_concepts = processor.find_key_concepts(result, group_size, verbose=True)
+    
     return {
-        "summary": summary
+        "key_concepts": key_concepts
     }
-
-    # -------------- TASK 4 ------------------
-    # loader = YoutubeLoader.from_youtube_url(str(request.youtube_link), add_video_info = True)
-    # docs = loader.load()
-    # print(f"On load: {type(docs)}")
-    # text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 0)
-    # result = text_splitter.split_documents(docs)
-
-    # print(f"{type(result)}")
-    # author = result[0].metadata['author']
-    # length = result[0].metadata['length']
-    # title = result[0].metadata['title']
-    # total_size = len(result)
-
-    # return {
-    #     "author": author,
-    #     "length": length,
-    #     "title": title,
-    #     "total_size": total_size,
-    # }
-
-    # # Doing the analysis
-    # processor = YoutubeProcessor(genai_processor = genai_processor)
-    # result = processor.retrieve_youtube_documents(str(request.youtube_link), verbose=False)
-    
-    # #summary = genai_processor.generate_document_summary(result, verbose=True)
-    
-    # # Find key concepts
-    # raw_concepts = processor.find_key_concepts(result, verbose=True)
-    
-    # # Deconstruct
-    # unique_concepts = {}
-    # for concept_dict in raw_concepts:
-    #     for key, value in concept_dict.items():
-    #         unique_concepts[key] = value
-    
-    # # Reconstruct
-    # key_concepts_list = [{key: value} for key, value in concept_dict.items()]
-    
-    # return {
-    #     "key_concepts": key_concepts_list,
-    # }
